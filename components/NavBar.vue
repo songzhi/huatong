e11d2b00483a84a0908a9e6928d0aa124a87d856
lsongzhi <lsongzhi@163.com>
2020-04-01 17:42:26 +0800

    feat: navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,37 +1,36 @@
 <template>
-  <b-navbar
-    transparent
-    fixed-top
-    class="has-text-white"
-  >
-    <template v-slot:brand>
-      <b-navbar-item
-        tag="router-link"
-        class="has-text-white"
-        :to="{ path: '/' }"
-      >
-        <img
-          src="/logo.png"
-          alt="星际华通科技公司"
+  <header :class="{'has-bg': isHasBg}">
+    <b-navbar>
+      <template v-slot:brand>
+        <b-navbar-item
+          tag="router-link"
+          :to="{ path: '/' }"
         >
-        星际华通科技公司
-      </b-navbar-item>
-    </template>
-    <template v-slot:start>
-      <b-navbar-item
-        v-for="item in navItems"
-        :key="item"
-        :href="'#'+item"
-      >
-        {{item}}
-      </b-navbar-item>
-    </template>
-  </b-navbar>
+          <img
+            src="/logo.png"
+            alt="星际华通科技公司"
+          >
+          星际华通科技公司
+        </b-navbar-item>
+      </template>
+      <template v-slot:start>
+        <b-navbar-item
+          v-for="item in navItems"
+          :key="item"
+          :href="'#'+item"
+        >
+          {{item}}
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+  </header>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { Prop, Component, Watch } from 'vue-property-decorator'
+// @ts-ignore
+import { throttle } from 'throttle-debounce';
 
 @Component({})
 export default class NavBar extends Vue {
@@ -43,15 +42,41 @@ export default class NavBar extends Vue {
     '招商加盟',
     '联系我们'
   ]
+  isHasBg = false
+  onScroll() {
+    const scrollTop = document.body.scrollTop + document.documentElement.scrollTop;
+    const contentTop = document.getElementById('产品与服务')!.offsetTop
+    this.isHasBg = scrollTop > (contentTop - 64)
+  }
+  mounted() {
+    const onScroll = throttle(100, () => this.onScroll())
+    window.addEventListener("scroll", onScroll);
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .navbar {
   max-width: 1200px;
   margin: auto;
   height: 64px;
   background-color: transparent;
+}
+header {
+  height: 64px;
+  position: fixed;
+  top: 0;
+  z-index: 30;
+  width: 100%;
+  transition: all 0.5s;
+}
+header.has-bg {
+  background-color: white;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  .navbar-item {
+    color: #666;
+    font-weight: 300;
+  }
 }
 .navbar-menu {
   margin-left: 64px;
